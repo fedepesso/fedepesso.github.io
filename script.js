2efdde3045f912e2d1bf2edf3ff73deeb4500d5b
fedepesso <fpessina2002@gmail.com>
2020-03-14 17:24:13 +0100

    Aggiunta funzione per incremento dei punteggi e spostata la dichiarazione degli attributi onclick nell'html + altre modifiche minori

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ const rndbtn = document.getElementById("rnd")
 
 const show_hide = () => {
 	if (text.innerHTML===""){
-  text.innerHTML = "Press arrows to move, enter to fight, P to collect objects or weapons; you'll get a main menu page and an inventary box whenever you need it"
-}
-else{
-	text.innerHTML=""
-}
+  text.innerHTML = "Press arrows to move or fight, P to collect objects or weapons; with I you'll open an inventary box whenever you need it"
+	}
+	else{
+		text.innerHTML=""
+	}
 }
 
-const random = () => {
+const random_stats = () => {
 	for (let i=0; i<counter.length; i++){
 		counter[i].innerHTML= Math.floor(Math.random() * 11)
 	}
 }
 
-button.onclick = show_hide
-
-rndbtn.onclick = random
+const add_val = (stat, delta) => {
+	let element = document.getElementById(stat + '_val')
+	element.innerHTML = parseInt(element.innerHTML) + parseInt(delta);
+}
