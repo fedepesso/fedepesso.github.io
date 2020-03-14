1f8e495fbf4169fa09384c69ba24db184a62cb64
Simone Pucci <simopucci2002@gmail.com>
2020-03-14 16:27:27 +0100

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const button = document.getElementById("incrementa-btn")
-const counter = document.getElementById("contatore-lbl")
+const button = document.getElementById("instruction-btn")
+const button2 = document.getElementsByClassName("char-btn")
+const text = document.getElementById("txt-lbl")
+const counter = document.getElementsByClassName("contatore-lbl")
+const rndbtn = document.getElementById("rnd")
 
-let counterValue = 0
-const increment = () => {
-  counterValue ++
-  counter.innerHTML = counterValue
+
+const show_hide = () => {
+	if (text.innerHTML===""){
+  text.innerHTML = "Press arrows to move, enter to fight, P to collect objects or weapons; you'll get a main menu page and an inventary box whenever you need it"
+}
+else{
+	text.innerHTML=""
+}
 }
 
-button.onclick = increment
+const random = () => {
+	for (let i=0; i<counter.length; i++){
+		counter[i].innerHTML= Math.floor(Math.random() * 11)
+	}
+}
+
+button.onclick = show_hide
+
+rndbtn.onclick = random
