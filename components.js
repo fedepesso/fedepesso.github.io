25108e549272d6963bc943fcf99c983b273bc2c2
Tommaso Pagliani <tp060202@gmail.com>
2020-03-22 18:14:24 +0100

    Aggiunto archivio e modificati costruttori

--- components.js
+++ components.js
@@ -1,10 +1,10 @@
 class Entity {
-    constructor() {
-        this.name = undefined;
+    constructor(info = [null, null, null, null]) {
+        this.name = info[0];
         this.position = [0, 0];
-        this.char = undefined;
-        this.color = undefined;
-        this.solid = true;
+        this.char = info[1];
+        this.color = info[2];
+        this.solid = info[3];
     }
 }
 
@@ -23,49 +23,52 @@ class Stats {
 
 
 class Attacker {
-    constructor() {
-        this.damage = [0, 0];
-        this.crit = [0, 0];
-        this.stat_bonus = [undefined, 0];
-        this.damage_type = undefined;
-        this.range = 1;
+    constructor(info=[null, [null, null], [null, null], [null, null], null, null]){
+        this.name = info[0];
+        this.damage = [info[1][0], info[1][1]];
+        this.crit = [info[2][0], info[2][1]];
+        this.stat_bonus = [info[3][0], info[3][1]];
+        this.damage_type = info[4];
+        this.range = info[5];
     }
 }
 
 
 class Defender {
-    constructor() {
-        this.physical = 0;
-        this.elemental = 0;
-        this.arcane = 0;
+    constructor(info=[null, null, null, null]) {
+        this.name = info[0];
+        this.physical = info[1];
+        this.elemental = info[2];
+        this.arcane = info[3];
     }
 }
 
 
+
 class Wearable {
-    constructor() {
-        this.rarity = 0;
+    constructor(info=[null, null, null]) {
+        this.rarity = info[0];
         // weapon, body_armor, leg_armor, ring_armor
-        this.category = undefined;
+        this.category = info[1];
         // hammer, axe, ecc ecc
-        this.type = undefined;
+        this.type = info[2];
     }
 }
 
 
 class Inventory {
-    constructor() {
-        this.weapon = undefined;
-        this.body_armor = undefined;
-        this.leg_armor = undefined;
-        this.ring_armor = undefined;
+    constructor(info=[null, null, null, null]) {
+        this.weapon = info[0];
+        this.body_armor = info[1];
+        this.leg_armor = info[2];
+        this.ring_armor = info[3];
     }
 }
 
 
 class Monster {
-    constructor() {
-        drop_tables = [];
+    constructor(info = [null]) {
+        drop_tables = info[0];
     }
 }
 
--- data.js
+++ data.js
@@ -1,11 +1,26 @@
-let weapons = {}
+let Oggetti = {
 
-let body_armors = {}
+    weapons : {
+        "spadone" : ["Spadone", [20, 30], [30, 2], ["strength", 20], "physical", 1],
+    },
 
-let leg_armors = {}
+    body_armors : {
+        "armatura a scalie" : ["Armatura a Scalie", 10, 10, 10],
+    },
 
-let rings = {}
+    leg_armors : {
+        "gambali da gallina" : ["Gambali da Gallina", -2, -2, -2],
+    },
 
-let monster_spawns = {}
+    rings : {},
 
-let drop_tables = {}
+    monster_spawns : {},
+
+    drop_tables : {}
+}
+
+
+
+const obtain_Object = function(type, name){
+    return Oggetti[type][name];
+}
